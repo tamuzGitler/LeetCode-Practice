581288c76fc3d6dccfae29dd424b1ab01c7597e3
Tamuz Huji <tamuz.gitler@mail.huji.ac.il>
2024-05-11 17:43:29 +0300

    solved

--- CoinChange.py
+++ CoinChange.py
@@ -9,22 +9,19 @@ class Solution(object):
         :rtype: int
         """
         i = len(coins) - 1
-        count = 0
         coins.sort()
         table = [0] * (amount + 1)
-        for i in range(1,len(table)):
+        for i in range(1, len(table)):
             if i in coins:
                 table[i] = 1
             else:
                 min_coins = math.inf
                 for coin in coins:
                     if i - coin >= 0:
-                        if table[i-coin] != -1:
-                            min_coins = min(table[i-coin] + 1,min_coins)
+                        if table[i - coin] != -1:
+                            min_coins = min(table[i - coin] + 1, min_coins)
                 table[i] = min_coins if min_coins != math.inf else -1
         return table[-1]
-
-
 
 
 if __name__ == '__main__':
--- WordSearch.py
+++ WordSearch.py
@@ -5,27 +5,36 @@ class Solution(object):
         :type word: str
         :rtype: bool
         """
-        return self.helper(board, word, 0, 0, 0, used=set())
+        visited = set()
+        for i in range(len(board)):
+            for j in range(len(board[0])):
 
-    def helper(self, board, word, i, row, col, used):
-        if i >= len(word):
+                if self.helper(board, word, i, j, visited):  # every starting pos
+                    return True
+        return False
+
+    def helper(self, board, word, i, j, visited):
+        if len(word) == 0:
             return True
-        elif (row, col) in used:
+        if not self.legalIndex(board, i, j) or word[0] != board[i][j] or (i, j) in visited:
             return False
-        used.add((row, col))
+        visited.add((i, j))
+        if len(word) == 1:
+            return True
+        word = word[1:]
+        res = self.helper(board, word, i - 1, j, visited) or self.helper(board, word, i + 1, j,
+                                                                         visited) or self.helper(board, word, i, j - 1,
+                                                                                                 visited) or self.helper(
+            board, word, i, j + 1, visited)
+        visited.remove((i, j))
+        return res
 
-        if (row < 0 or col < 0 or row >= len(board) or col >= len(board[0])):
-            return False
-        if board[row][col] == word[0]:
-            i += 1
+    def legalIndex(self, board, i, j):
+        return ((i >= 0) and i < len(board)) and (j >= 0 and j < len(board[0]))
 
-        return (self.helper(board, word, i, row - 1, col, used) or self.helper(board, word, i, row + 1, col,
-                                                                               used) or
-                self.helper(board, word, i, row, col - 1, used) or self.helper(board, word, i, row, col + 1,
-                                                                               used))
 
 if __name__ == '__main__':
     board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
     word = "ABCCED"
     sol = Solution()
-    print(sol.exist(board,word))
+    print(sol.exist(board, word))
